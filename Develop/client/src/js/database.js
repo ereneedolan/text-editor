18f0ac7a43fdf981a4aee728cee19070aa3663af
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-04 19:55:30 -0500

    naming with jate

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,9 +15,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 console.log('PUT to the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readwrite');
-  const store = tx.objectStore('todos');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   const request = store.put({ id: id, todo: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -25,9 +25,9 @@ console.log('PUT to the database');
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 console.log('GET from the database');
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.get(id);
   const result = await request;
   console.log('result.value', result);
