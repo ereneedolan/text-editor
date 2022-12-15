796c1b70bddeb1a85a0d2278c1794429d4326a57
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-14 22:18:35 -0500

    got caching working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {console.error('putDb not implemented');
+export const putDb = async (content) => {
 console.log('PUT to the database');
   const noteDb = await openDB('jate', 1);
   const tx = noteDb.transaction('jate', 'readwrite');
@@ -23,15 +23,15 @@ console.log('PUT to the database');
   console.log('🚀 - data saved to the database', result);
 }
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {console.error('getDb not implemented');
+export const getDb = async () => {
 console.log('GET from the database');
   const noteDb = await openDB('jate', 1);
   const tx = noteDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;}
+  return result.value;}
 
 
 initdb();
